e87dd0c9f6426d99fdf14aad67c2ef966b8d8735
Peter Pettersson <peter.pettersson@protonmail.com>
2025-05-28 11:58:17 +0200

    Feature/wellknown configuration (#25)
    * add wellknown arg
    
    * test
    
    * Fix test and update nock version
    
    ---------
    
    Co-authored-by: Mikus Vanags (irmalien) <mikus.vanags@bonniernews.se>
    Co-authored-by: hanna.binder <hanna.binder@bonniernews.se>

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -3,20 +3,42 @@ import { Router as createRouter, type Router } from "express";
  * Express middleware to be used to connect to Bonnier OIDC provider and register reguired routes.
  */
 
-interface ClientConfig {
+type ClientConfig = {
   clientId: string;
-}
+  issuerBaseURL: string;
+};
 
-export const middleware = (clientConfig: ClientConfig): Router => {
+type OIDCWellKnownConfig = {
+  issuer: string;
+  authorization_endpoint: string;
+  token_endpoint: string;
+  userinfo_endpoint: string;
+  jwks_uri: string;
+  end_session_endpoint: string;
+  scopes_supported: string[];
+  response_types_supported: string[];
+  grant_types_supported: string[];
+  subject_types_supported: string[];
+  id_token_signing_alg_values_supported: string[];
+  ui_locales_supported: string[];
+};
+
+export const middleware = async (clientConfig: ClientConfig): Promise<Router> => {
   const router = createRouter();
-  const { clientId } = clientConfig;
+  const { clientId, issuerBaseURL } = clientConfig;
+
+  const response = await fetch(`${issuerBaseURL}/oauth/.well-known/openid-configuration`);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const wellknownConfig: OIDCWellKnownConfig = await response.json();
+
   router.use((_req, _res, next) => {
     // do any required setup
     return next();
   });
 
   router.post("/callback", (_req, res) => {
-    res.send(` Callback received for client ID: ${clientId}`);
+    res.send(`Callback received for client ID: ${clientId}`);
   });
 
   return router;
