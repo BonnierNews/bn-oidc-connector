2b0566b768c15eb615e18190865b9b9ed417fe4b
Peter Pettersson <peter.pettersson@bonniernews.se>
2025-07-08 09:04:46 +0200

    fix(test): adjust import and remove comments

--- test/unit/setup.unit.ts
+++ test/unit/setup.unit.ts
@@ -1,23 +1,21 @@
-// import { type Request, type Response } from "express";
 import nock from "nock";
 
-import { auth, type OidcClientConfig } from "../../index";
+import { auth } from "../../index";
 import { initialize } from "../../lib/auth";
+import type { OidcClientConfig } from "../../lib/types";
 
-// import { handleCallback } from "../../lib/callback";
-// import { handleLogin } from "../../lib/login";
-// import { createApp, createAppWithMiddleware } from "../helpers/app-helper";
-
-// const clientId = "test-client-id";
-// const issuerBaseURL = "https://oidc.test";
-// const baseURL = "http://test.example";
+const clientId = "test-client-id";
+const issuerBaseURL = "https://oidc.test";
+const baseURL = "http://test.example";
 
 Feature("Setup", () => {
   Scenario("Middleware is not initialized", () => {
     let config : any;
+
     Given("config is missing required params", () => {
       config = {};
     });
+
     let error: Error | null = null;
     When("creating oidc middleware without config", () => {
       try {
@@ -26,28 +24,14 @@ Feature("Setup", () => {
         error = err as Error;
       }
     });
+
     Then("an error is thrown", () => {
       expect(error).to.be.an.instanceOf(Error);
       expect(error?.message).to.equal("OIDC client config is missing required parameters");
     });
   });
 
-  // Scenario("Initializing middleware with invalid config", () => {
-  //   let config: Partial<OidcClientConfig> | null = null;
-  //   When("initializing middleware with invalid config", () => {
-  //     config = {};
-  //   });
-  //
-  //   Then("an error is thrown", () => {
-  //     expect(() => auth(config as OidcClientConfig)).to.throw("OIDC client config is missing required parameters");
-  //   });
-  // });
-
   Scenario("Middleware fails initialization", () => {
-    const clientId = "test-client-id";
-    const issuerBaseURL = "https://oidc.test";
-    const baseURL = "http://test.example";
-
     Given("the OIDC provider cannot be reached", () => {
       nock(issuerBaseURL)
         .get("/oauth/.well-known/openid-configuration")
