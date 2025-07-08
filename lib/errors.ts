46d5ec0540e0a424eea66ceefd1d9cdc6db9063f
Kit Gustavsson (BN) <kit.gustavsson@bonniernews.se>
2025-07-08 16:23:32 +0200

    add isAuthenticated middleware

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,3 +1,4 @@
 /* eslint-disable @bonniernews/typescript-rules/disallow-class-extends */
 export class OidcError extends Error {}
 export class UnauthorizedError extends OidcError {}
+export class UnauthenticatedError extends OidcError {}
---
+++ lib/middleware/is-authenticated.ts
@@ -0,0 +1,15 @@
+import type { Request, Response, NextFunction } from "express";
+
+import { UnauthenticatedError } from "../errors";
+
+function isAuthenticated() {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    if (req.oidc.isAuthenticated) {
+      return next();
+    }
+
+    next(new UnauthenticatedError("User is not logged in"));
+  };
+}
+
+export { isAuthenticated };
